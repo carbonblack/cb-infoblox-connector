8da60a222cf3acd09d3732d66f525040a855fc27
Zachary Estep <zestep@carbonblack.com>
2018-01-08 13:14:07 -0500

    Cleaning up logging

--- cbinfoblox/restapi_poller.py
+++ cbinfoblox/restapi_poller.py
@@ -46,13 +46,10 @@ class RestPoller(threading.Thread):
 
                 # go to sleep till we another increment passes
 
-                logger.info("Sleeping for time increment + {}".format(self.TIME_INCREMENT))
                 time.sleep(self.TIME_INCREMENT.total_seconds())
-                logger.info("Done sleeping!")
 
         except:
             logger.error('%s' % traceback.format_exc())
-            logger.info("%s" % traceback.format_exc())
         logger.info("Rest api poller existing")
 
 
